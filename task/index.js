729e7ccba5479645606903aee1101ab5034121df
Hernan Dario Castano Rueda <hecastan@bancolombia.com.co>
2023-09-20 15:57:17 -0500

    Fix value field serialization

--- task/index.js
+++ task/index.js
@@ -8,17 +8,58 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("azure-pipelines-task-lib/task");
+const axios_1 = __importDefault(require("axios"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const inputString = tl.getInput('samplestring', true);
-            if (inputString == 'bad') {
-                tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-                return;
-            }
-            console.log('Hello', inputString);
+            const environmentId = tl.getInput('environmentId', true);
+            const apiToken = tl.getInput('apiToken', true);
+            const externalId = tl.getInput('externalId', false);
+            const insertAfter = tl.getInput('insertAfter', false);
+            const objectId = tl.getInput('objectId', false);
+            const schemaId = tl.getInput('schemaId', true);
+            const schemaVersion = tl.getInput('schemaVersion', false);
+            const scope = tl.getInput('scope', true);
+            const value = tl.getInput('value', true);
+            const payload = JSON.stringify([
+                {
+                    "externalId": externalId,
+                    "insertAfter": insertAfter,
+                    "objectId": objectId,
+                    "schemaId": schemaId,
+                    "schemaVersion": schemaVersion,
+                    "scope": scope,
+                    "value": JSON.parse(value)
+                }
+            ]);
+            const config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: `https://${environmentId}.live.dynatrace.com/api/v2/settings/objects`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Api-Token ${apiToken}`
+                },
+                data: payload
+            };
+            axios_1.default.request(config)
+                .then((response) => {
+                console.log(JSON.stringify(response.data));
+                if (response.status != 200) {
+                    tl.setResult(tl.TaskResult.Failed, "Response status code not OK");
+                }
+                else {
+                    tl.setResult(tl.TaskResult.Succeeded);
+                }
+            })
+                .catch((error) => {
+                tl.setResult(tl.TaskResult.Failed, error.message);
+            });
         }
         catch (err) {
             tl.setResult(tl.TaskResult.Failed, err.message);
--- task/index.ts
+++ task/index.ts
@@ -1,14 +1,53 @@
 import tl = require('azure-pipelines-task-lib/task');
-import axios = require('axios');
+import axios from 'axios';
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
-        }
-        console.log('Hello', inputString);
+        const environmentId: string | undefined = tl.getInput('environmentId', true);
+        const apiToken: string | undefined = tl.getInput('apiToken', true);
+        const externalId: string | undefined = tl.getInput('externalId', false);
+        const insertAfter: string | undefined = tl.getInput('insertAfter', false);
+        const objectId: string | undefined = tl.getInput('objectId', false);
+        const schemaId: string | undefined = tl.getInput('schemaId', true);
+        const schemaVersion: string | undefined = tl.getInput('schemaVersion', false);
+        const scope: string | undefined = tl.getInput('scope', true);
+        const value: string | undefined = tl.getInput('value', true);
+
+        const payload = JSON.stringify([
+            {
+               "externalId": externalId,
+               "insertAfter": insertAfter,
+               "objectId": objectId,
+               "schemaId": schemaId,
+               "schemaVersion": schemaVersion,
+               "scope": scope,
+               "value": JSON.parse(value!)
+            }  
+        ]);
+
+        const config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: `https://${environmentId}.live.dynatrace.com/api/v2/settings/objects`,
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Authorization': `Api-Token ${apiToken}`
+              },
+            data: payload
+        };
+
+        axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          if (response.status != 200) {
+            tl.setResult(tl.TaskResult.Failed, "Response status code not OK");
+          } else {
+            tl.setResult(tl.TaskResult.Succeeded);
+          }
+        })
+        .catch((error) => {
+            tl.setResult(tl.TaskResult.Failed, error.message);
+        });
     }
     catch (err: any) {
         tl.setResult(tl.TaskResult.Failed, err.message);
